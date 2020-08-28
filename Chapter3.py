1feeba37f9c18845da193637a33239977c8c8fb7
michael621anthony@gmail.com <michael621anthony@gmail.com>
2020-08-28 01:40:34 -0700

    Finished Chapter3. Local and global variables and try and except clauses.

---
+++ Chapter3.py
@@ -0,0 +1,47 @@
+import time, sys
+spam = print('Hello!')
+
+print(None == spam)
+#return will output 'None' value if not followed by anything
+print('Hello', end='')#end is a keyword
+print('World')
+
+print('cats', 'dogs', 'mice')
+print('cats', 'dogs', 'mice', sep=',')#sep is a keyword that adds a seperator
+
+def spam(divideBy):
+    try:
+        return 42 / divideBy
+    except ZeroDivisionError:
+        print('Error: Invalid argument.')
+
+print(spam(2))
+print(spam(12))
+print(spam(0))
+print(spam(1))
+
+
+indent = 0 # How many spaces to indent.
+indentIncreasing = True # Whether the indentation is increasing or not.
+
+try:
+    while True: # The main program loop.
+        print(' ' * indent, end='')
+        print('********')
+        time.sleep(0.1) # Pause for 1/10 of a second.
+
+        if indentIncreasing:
+            # Increase the number of spaces:
+            indent = indent + 1
+            if indent == 20:
+                # Change direction:
+                indentIncreasing = False
+
+        else:
+            # Decrease the number of spaces:
+            indent = indent - 1
+            if indent == 0:
+                # Change direction:
+                indentIncreasing = True
+except KeyboardInterrupt:
+    sys.exit()
